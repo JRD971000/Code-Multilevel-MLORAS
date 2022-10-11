fix scaling

gen_data.py
@@ -121,4 +121,14 @@ def generate_data(data_args, show_fig = False):
             
     torch.save(data_args, path+"/data_config.pth")
             
-generate_data(data_args, show_fig = False)
+# generate_data(data_args, show_fig = False)
+
+
+for i in range(17):
+    print(i)
+    g = torch.load('Data/test_grids/grid'+str(i)+'.pth')
+    grid = Grid_PWA(g.A, g.mesh, 0.2, hops = 1, 
+                      cut=1, h = 1, nu = 0, BC = 'Dirichlet')    
+    ratio = max(25*((g.A.shape[0]/600)**0.5)/g.A.shape[0], 2/g.A.shape[0])
+    grid.aggop_gen(ratio, 1)
+    torch.save(grid, 'Data/test_grids_2/grid'+str(i)+'.pth')

grids.py
@@ -754,18 +754,18 @@ class Grid_PWA():
         else:
             
             self.aggop = node_agg
-            
+        
         num_aggs = self.aggop[0].shape[1]
         list_aggs = {}
         list_cut_aggs = {}
         for i in range(num_aggs):
             list_aggs[i] = []
             list_cut_aggs[i] = set([])
-        
+
         for i, n in enumerate(self.aggop[-1]):
             list_aggs[n].append(i)
             list_cut_aggs[n] = list_cut_aggs[n].union(self.dict_nodes_neighbors_cut[i])
-        
+
         for i in range(num_aggs):
             list_cut_aggs[i] = list(list_cut_aggs[i])
 
@@ -774,13 +774,14 @@ class Grid_PWA():
         for i in range(num_aggs):
             learn_nodes[i] = list(set(list_cut_aggs[i]) - set(list_aggs[i]))
             boarder_hops.extend(learn_nodes[i])
-            
-        list_learn_edges = {}
 
-        for i in range(num_aggs):
-            list_learn_edges[i] = list(self.networkx.subgraph(learn_nodes[i]).edges())
+        # list_learn_edges = {}
+
+        # for i in range(num_aggs):
+        #     list_learn_edges[i] = list(self.networkx.subgraph(learn_nodes[i]).edges())
 
         mask_edges = []
+        
         
         if self.hops != -1:
             for i in range(num_aggs):
@@ -801,22 +802,28 @@ class Grid_PWA():
         
         mask_mat = scipy.sparse.csr_matrix((np.ones(len(mask_edges)), (np.array(mask_edges)[:,0].tolist(), np.array(mask_edges)[:,1].tolist())), shape=(sz, sz))
 
-           
-        all_eye = np.eye(sz)
 
+        # all_eye = np.eye(sz)
+                
         R = {}
         R_hop = {}
         modified_R = {}
         
         for i in range(num_aggs):
-            R[i] = scipy.sparse.csr_matrix(all_eye[list_aggs[i], :])
-            R_hop[i] = scipy.sparse.csr_matrix(all_eye[list_cut_aggs[i], :])
+            # R[i] = scipy.sparse.csr_matrix(all_eye[list_aggs[i], :])
+            R_nodes = len(list_aggs[i])
+            R[i] = scipy.sparse.csr_matrix((np.ones(R_nodes), (np.arange(R_nodes).tolist(), list_aggs[i])), shape=(R_nodes, sz))
             
+            # R_hop[i] = scipy.sparse.csr_matrix(all_eye[list_cut_aggs[i], :])
+            R_hop_nodes = len(list_cut_aggs[i])
+            R_hop[i] = scipy.sparse.csr_matrix((np.ones(R_hop_nodes), (np.arange(R_hop_nodes).tolist(), list_cut_aggs[i])), shape=(R_hop_nodes, sz))
+
             list_ixs = []
             for e in list_aggs[i]:
                 list_ixs.append(list_cut_aggs[i].index(e))
         
-            modified_R[i] = scipy.sparse.csr_matrix((np.ones(len(list_aggs[i])), (list_ixs, np.array(list_cut_aggs[i])[list_ixs])), shape=(len(list_cut_aggs[i]), sz))
+            modified_R[i] = scipy.sparse.csr_matrix((np.ones(R_nodes), (list_ixs, np.array(list_cut_aggs[i])[list_ixs])), shape=(R_hop_nodes, sz))
+        
         
         l0 = []
         l1 = []
@@ -830,11 +837,27 @@ class Grid_PWA():
         R0 = scipy.sparse.diags(1/R0.sum(axis=1).A.ravel()) @ R0
         R0 = R0.transpose()
         
+        
+        
+        A_c = self.aggop[0].transpose() @ self.A @ self.aggop[0]
+        l0 = []
+        l1 = []
+        for i in range(num_aggs):
+
+            non_zeros = R_hop[i].nonzero()[-1].tolist()
+            for neigh in A_c[i].nonzero()[-1].tolist():
+                l0.extend(non_zeros)
+                l1.extend([neigh for j in range(len(non_zeros))]) 
+
+        neigh_R0 = scipy.sparse.csr_matrix((np.ones(len(l0)), (l0, l1)), shape=(sz, num_aggs))
+        neigh_R0 = scipy.sparse.diags(1/neigh_R0.sum(axis=1).A.ravel()) @ neigh_R0
+        neigh_R0 = neigh_R0.transpose()
+        
             
         self.list_aggs = list_aggs
         self.list_cut_aggs = list_cut_aggs
         self.learn_nodes = learn_nodes
-        self.list_learn_edges = list_learn_edges
+        # self.list_learn_edges = list_learn_edges
         self.mask_edges = mask_edges
         self.gmask = mask_mat
         self.boarder_hops = boarder_hops
@@ -842,10 +865,12 @@ class Grid_PWA():
         self.R_hop = R_hop
         self.modified_R = modified_R
         self.R0 = R0
+        self.neigh_R0 = neigh_R0
         self.gdata = self.data()
 
         if self.BC == 'Dirichlet':
             self.apply_bc(1e-16)
+
 
         return 
     

hgnn.py
@@ -19,7 +19,7 @@ from torch_geometric.datasets import Planetoid
 from torch_geometric.utils import subgraph
 from torch_geometric.utils.convert import to_networkx
 from torch_geometric.nn import HeteroConv, GCNConv, SAGEConv, GATConv, Linear,\
-                                 GraphUNet, TAGConv, MessagePassing
+                                 GraphUNet, TAGConv, MessagePassing, NNConv
 import sys
 import scipy
 import torch_geometric
@@ -49,9 +49,11 @@ def make_sparse_torch(A, sparse = True):
 
 
 class PlainMP(MessagePassing):
-    def __init__(self, aggr = 'mean'):
+    def __init__(self, dim_embed, aggr = 'add'):
         super().__init__(aggr=aggr) #  "Max" aggregation.
- 
+        self.net = torch.nn.Sequential(Linear(2*dim_embed, dim_embed), 
+                                # torch.nn.ReLU(), Linear(dim_embed, dim_embed),
+                                torch.nn.ReLU(), Linear(dim_embed, dim_embed))
 
     def forward(self, x, edge_index):
         # x has shape [N, in_channels]
@@ -60,8 +62,10 @@ class PlainMP(MessagePassing):
         return self.propagate(edge_index, x=x)
 
     def message(self, x_i, x_j):
-
-        return x_i
+        
+        x = torch.cat((x_i, x_j), dim = 1)
+        x = self.net(x)
+        return x#x_i
 
 class GUNET(torch.nn.Module):
     def __init__(self, in_dim, dim, out_dim, depth, lr):
@@ -130,7 +134,14 @@ def K_means_agg_torch(X, A, ratio, grid):
     # X_coarse = (X.t() @ tsR.to_dense()).t()
 
     
-    fine2coarse = torch.tensor(np.int64(R.nonzero())).to(device)
+    # fine2coarse = torch.tensor(np.int64(R.nonzero())).to(device)
+    
+    neigh_R0 = grid.neigh_R0.transpose()
+    
+    fine2coarse = torch.tensor(np.int64(neigh_R0.nonzero())).to(device)
+    
+    
+
     # fine2coarse = tsR.to_dense().nonzero().to(device)
     # fine2coarse = fine2coarse.reshape(fine2coarse.shape[1], fine2coarse.shape[0])
     
@@ -178,11 +189,11 @@ class HGNN(torch.nn.Module):
             
             for i in range(lvl):
                 if i <lvl-1:
-                    dict_fc[('L'+str(i), '->', 'L'+str(i+1))] = PlainMP()#GATConv(dim_embed, dim_embed, edge_dim = dim_embed)#TAGConv(dim_embed, dim_embed, K = 2, normalize = False)#
-                    dict_cf[('L'+str(i+1), '->', 'L'+str(i))] = PlainMP()#GATConv(dim_embed, dim_embed, edge_dim = dim_embed)#TAGConv(dim_embed, dim_embed, K = 2, normalize = False)#
+                    dict_fc[('L'+str(i), '->', 'L'+str(i+1))] = PlainMP(dim_embed)#GATConv(dim_embed, dim_embed, edge_dim = dim_embed)#TAGConv(dim_embed, dim_embed, K = 2, normalize = False)#
+                    dict_cf[('L'+str(i+1), '->', 'L'+str(i))] = PlainMP(dim_embed)#GATConv(dim_embed, dim_embed, edge_dim = dim_embed)#TAGConv(dim_embed, dim_embed, K = 2, normalize = False)#
                     
-            conv_fc = HeteroConv(dict_fc, aggr='sum')
-            conv_cf = HeteroConv(dict_cf, aggr='sum')
+            conv_fc = HeteroConv(dict_fc, aggr='add')
+            conv_cf = HeteroConv(dict_cf, aggr='add')
             
             self.f2c.append(conv_fc)            
             self.c2f.append(conv_cf)
@@ -193,13 +204,24 @@ class HGNN(torch.nn.Module):
                     if i == 0 or i == lvl-1:
          
                         dict_ff[('L'+str(i), '-', 'L'+str(i))] = TAGConv(2*dim_embed, dim_embed, K = K, normalize = False)#GATConv(dim_embed, dim_embed, edge_dim = dim_embed)
+                        # dict_ff[('L'+str(i), '-', 'L'+str(i))] = NNConv(2*dim_embed, dim_embed, 
+                        #                         nn=torch.nn.Sequential(Linear(1, dim_embed), 
+                        #                         torch.nn.ReLU(), Linear(dim_embed, 2*dim_embed*dim_embed)), aggr = 'mean')
+
                     else:
                         dict_ff[('L'+str(i), '-', 'L'+str(i))] = TAGConv(3*dim_embed, dim_embed, K = K, normalize = False)#GATConv(dim_embed, dim_embed, edge_dim = dim_embed)
-            else:
+                        # dict_ff[('L'+str(i), '-', 'L'+str(i))] = NNConv(3*dim_embed, dim_embed, 
+                        #                         nn=torch.nn.Sequential(Linear(1, dim_embed), 
+                        #                         torch.nn.ReLU(), Linear(dim_embed, 3*dim_embed*dim_embed)), aggr = 'mean')
+
+            else:   
                 
                 dict_ff[('L'+str(0), '-', 'L'+str(0))] = TAGConv(dim_embed, dim_embed, K = K, normalize = False)#GATConv(dim_embed, dim_embed, edge_dim = dim_embed)
+                # dict_ff[('L'+str(0), '-', 'L'+str(0))] = NNConv(dim_embed, dim_embed, 
+                #                         nn=torch.nn.Sequential(Linear(1, dim_embed), 
+                #                         torch.nn.ReLU(), Linear(dim_embed, dim_embed*dim_embed)), aggr = 'mean')
 
-            conv_ff = HeteroConv(dict_ff, aggr='sum')
+            conv_ff = HeteroConv(dict_ff, aggr='add')
 
             self.convs.append(conv_ff)
             
@@ -212,6 +234,10 @@ class HGNN(torch.nn.Module):
         
         self.edge_model  = EdgeModel(dim_embed*2, [dim_embed, int(dim_embed/2), int(dim_embed/4)], 1)
         self.edge_model_R  = EdgeModel(dim_embed*2, [dim_embed, int(dim_embed/2), int(dim_embed/4)], 1)
+        
+        # self.edge_model_R_dense = torch.nn.Sequential(Linear(dim_embed, dim_embed), 
+        #                         torch.nn.ReLU(), Linear(dim_embed, dim_embed),
+        #                         torch.nn.ReLU(), Linear(dim_embed, 29))
 
         # self.network = torch_geometric.nn.Sequential(self.convs)
         
@@ -292,7 +318,7 @@ class HGNN(torch.nn.Module):
         x_ff = {key: x for key, x in x_dict.items()}
         
         for conv, conv_fc, conv_cf, normalization in zip(self.convs, self.f2c, self.c2f, self.normaliz):
-            
+
             if self.lvl > 1:
                 x_fc = conv_fc(x_ff, edge_index_dict)#, edge_attr_dict)
                 for key in x_fc.keys():
@@ -338,15 +364,21 @@ class HGNN(torch.nn.Module):
         row_coarse = edge_index_dict[('L1', '->', 'L0')][0].tolist()
         col_fine = edge_index_dict[('L1', '->', 'L0')][1].tolist()
         
+        
         edge_attr_R = self.edge_model_R(x_coarse[row_coarse], x[col_fine])
-
+                
         out_R = torch.sparse_coo_tensor([row_coarse, col_fine], edge_attr_R.flatten(),
                                                       (grid.R0.shape[0], grid.R0.shape[1])).double()
         
-        # out_R = out_R.to_dense()/out_R.to_dense().sum(0)
+        out_R = out_R.to_dense()/out_R.to_dense().sum(0)
 
-        # out_R = out_R.to_sparse()
-        
+        out_R = out_R.to_sparse()
+        '''
+        out_R = self.edge_model_R_dense(x_ff['L0']).t()
+        out_R = out_R/out_R.sum(0)
+
+        out_R = out_R.double().to_sparse()
+        '''
         row = np.array(grid.mask_edges)[:,0].tolist()
         col = np.array(grid.mask_edges)[:,1].tolist()
         

test.py
@@ -46,6 +46,7 @@ test_parser.add_argument('--cut', type=int, default=1, help='RAS delta')
 test_args = test_parser.parse_args()
 num_lvl = 2
 
+
 if num_lvl == 1:
     
     from utils_1L import *
@@ -91,9 +92,9 @@ def test_fgmres(grid, dict_precs, list_test):
 
 def test_stats(grid, dict_stats, list_test):
     
-    u = torch.rand(grid.A.shape[0],100).double()
+    u = torch.rand(grid.A.shape[0],1).double()
     u = u/(((u**2).sum(0))**0.5).unsqueeze(0)
-    K = 100
+    K = 10
     
     
     dict_enorm = {}
@@ -107,192 +108,227 @@ def test_stats(grid, dict_stats, list_test):
             dict_enorm[name] = test_stationary(grid, None, precond_type = None, u = u, K = K, M = MJ)
         
         else:
-            dict_enorm[name] = test_stationary(grid, None, precond_type = name, u = u, K = K, M = dict_stats[name]) 
+            dict_enorm[name] = test_stationary(grid, dict_stats[name], precond_type = name, u = u, K = K, M = dict_stats[name][0]) 
             
     return dict_enorm
 
 
 if __name__ =='__main__':
     
-    # if test_args.structured:
-        
-    #     ratio = test_args.ratio
-    #     n = test_args.size_structured
-        
-    #     if test_args.BC == 'Dirichlet':
-    #         old_g = structured(n, n, Neumann=False)
-    #     else:
-    #         old_g = structured(n, n, Neumann=True)  
+        if test_args.structured:
             
-    #     grid =  Grid_PWA(old_g.A, old_g.mesh, test_args.ratio, hops = test_args.hops, 
-    #                       cut=test_args.cut, h = 1/(n+1), nu = 1)#, BC = 'Dirichlet')
-    #     grid.aggop_gen(ratio = 0.1, cut = 1, node_agg = struct_agg_PWA(n,n,int(n/2),n))
-        
-    # else:
-
-    #     lcmin = 0.04#np.random.uniform(0.08, 0.09)
-    #     lcmax = 0.045#np.random.uniform(0.14, 0.15)
-    #     n = np.random.choice([3,4,5,6,7,8,9,10,20,40])
-    #     randomized = True if np.random.rand() < 0.4 else True
-    #     old_g = rand_grid_gen1(randomized = randomized, n = n, min_ = 0.03, min_sz = 0.6, 
-    #                   lcmin = lcmin, lcmax = lcmax, distmin = 0.01, distmax = 0.035, PDE = test_args.PDE)
-        
-    #     print(old_g.A.shape[0])
-    #     test_args.ratio = 25*((old_g.A.shape[0]/600)**0.5)/old_g.A.shape[0]
-    #     grid =  Grid_PWA(old_g.A, old_g.mesh, test_args.ratio, hops = test_args.hops, 
-    #                       cut=test_args.cut, h = 1, nu = 0, BC = test_args.BC)                
-    
-       
-    # grid = torch.load('Data/TG5k/grid10.pth')
-    # gg = Grid_PWA(grid.A, grid.mesh, ratio = 0.3, hops=-1)
-    # gg.aggop_gen(grid.ratio, grid.cut, grid.aggop)
-    grid = torch.load('grid1.pth')
-    
-    # A0 = grid.R0 @ grid.A @ grid.R0.transpose()
-    # Cratio = max(2/A0.shape[0], 12*((A0.shape[0]/600)**0.5)/A0.shape[0])
-    # Cgrid = Grid_PWA(A0, grid.mesh,Cratio)  
-    # grid = torch.load('Data/testdata/grid9.pth')
-    # grid.global_Lap_eig()
-    
-    the_types = 1
-        
-    if the_types == 0:
-
-        grid.aggop_gen(0.02, 1)
-
-    if the_types == 2:
-
-        grid.aggop_gen(35/grid.A.shape[0], 1)
-    
-    # grid = torch.load('data/Grids-Helmholtz-Dirichlet/grid90.pth')
-    # gg = Grid_PWA(grid.A, grid.mesh, ratio = grid.ratio, hops=1)
-    # gg.aggop_gen(grid.ratio, grid.cut, grid.aggop)
-    # grid =  gg
-    
-    if test_args.plot:
-
-        grid.plot_agg(size = 0.0, labeling = False, w = 0.1, shade=0.0008)
-        # plt.savefig('../Paper-Multilevel-MLORAS/1_submitted_paper/figures/overTgrid.pdf', bbox_inches = 'tight')
-
-        plt.show()
-
-    ts = time.time()
-
-    from NeuralNet import *
-    from hgnn import HGNN
-    model = HGNN(lvl=2, dim_embed=128, num_layers=6, K= 6, ratio=0.025, lr=1e-4)
-    # model = mloras_net(dim = 128, K = 2, num_res = 8, num_convs = 4, lr = 1e-3, res = True, tf=False)
-    
-    directory  = 'Models/Model-TG-new/model_epoch_best.pth'
-    
-    
-    # from PrevNeuralNet import mloras_net
-    # model = mloras_net(dim = test_args.dim, K = test_args.TAGConv_k, num_res = 8, num_convs = 4, lr = 1e-4)
-
-    # directory  = 'Models/Model-Grids-Helmholtz-Dirichlet/trained_model.pth'
-
-    # from NN_experiment import FC_test
-    # model = FC_test(grid.A.shape[0], grid.gmask.nonzero()[0].shape[0], 128, lr = 1e-4)
-
-    # directory  = 'Models/Model-gnn-all/model_epoch_best.pth'
-    # directory  = 'Models/Model-NN-test/model_epoch989.pth'
-
-
-    model.load_state_dict(torch.load(directory, map_location=torch.device('cpu')))
-    
-    list_test = ['RAS', 'ML_ORAS']
-    list_label = {'RAS':'RAS','ML_ORAS': 'MLORAS'}
-    dict_precs = {}
-    dict_stats = {}
-    n = grid.aggop[0].shape[0]
-    
-    x0 = np.random.random(grid.A.shape[0])
-    x0 = x0/((grid.A@x0)**2).sum()**0.5
-    
-    data = grid.gdata
-    data.edge_attr = data.edge_attr.float()
-    model.eval()
-    with torch.no_grad():
-        out = model(data, grid)
-    
-    for name in list_test:
-        
-        if num_lvl == 1:
-            M = preconditioner(grid, out, precond_type=name, u = torch.tensor(x0).unsqueeze(1))
+            ratio = test_args.ratio
+            n = test_args.size_structured
             
-            dict_precs[name] = M.numpy()
-            dict_stats[name] = M
-        
-        if num_lvl == 2:
-            Mstat, Mprec = preconditioner(grid, out, precond_type=name, u = torch.tensor(x0).unsqueeze(1))
-            if name == 'ML_ORAS':
-                dict_precs[name] = [Mprec.to_dense().detach().numpy(), out[1]]
+            if test_args.BC == 'Dirichlet':
+                old_g = structured(n, n, Neumann=False)
             else:
-                dict_precs[name] = [Mprec.to_dense().detach().numpy(), None]
+                old_g = structured(n, n, Neumann=True)  
                 
-            dict_stats[name] = Mstat
+            grid =  Grid_PWA(old_g.A, old_g.mesh, test_args.ratio, hops = test_args.hops, 
+                              cut=test_args.cut, h = 1/(n+1), nu = 1)#, BC = 'Dirichlet')
+            grid.aggop_gen(ratio = 0.1, cut = 1, node_agg = struct_agg_PWA(n,n,int(n/2),n))
             
-        if num_lvl == 3:
-            
-            grids = [grid, Cgrid]
-            
-            Cdata = Cgrid.gdata
-            Cdata.edge_attr = Cdata.edge_attr.float()
-            model.eval()
-            with torch.no_grad():
-                Cout = model(Cdata, Cgrid)
-                
-                
-            outs  = [out, Cout]
-            
-            M = preconditioner(grids, outs, precond_type=name, u = torch.tensor(x0).unsqueeze(1))
-            
-            dict_precs[name] = M.numpy()
-            dict_stats[name] = M
-        
+        else:
     
-    if test_args.precond:
-        t3 = time.time()
-        dict_loss = test_fgmres(grid, dict_precs, list_test)
-        t4 = time.time()
-        print(f't43 = {t4-t3}\n')
+            lcmin = 0.0405#np.random.uniform(0.08, 0.09)
+            lcmax = 0.0415#np.random.uniform(0.14, 0.15)
+            n = np.random.choice([3,4,5,6,7,8,9,10,20,40])
+            randomized = True if np.random.rand() < 0.4 else True
+            old_g = rand_grid_gen1(randomized = randomized, n = n, min_ = 0.03, min_sz = 0.6, 
+                          lcmin = lcmin, lcmax = lcmax, distmin = 0.01, distmax = 0.035, PDE = test_args.PDE)
+            
+            print(old_g.A.shape[0])
+            test_args.ratio = 25*((old_g.A.shape[0]/600)**0.5)/old_g.A.shape[0]
+            grid =  Grid_PWA(old_g.A, old_g.mesh, test_args.ratio, hops = test_args.hops, 
+                              cut=test_args.cut, h = 1, nu = 0, BC = test_args.BC)                
+        
+           
+        # grid = torch.load('Data/data-800-1k/grid151.pth')
+        # gg = Grid_PWA(grid.A, grid.mesh, ratio = 0.3, hops=-1)
+        # gg.aggop_gen(grid.ratio, grid.cut, grid.aggop)
+        # grid = torch.load('Data/TrainingGrids/grid9.pth')
+        # grid = torch.load('grid_43k.pth')
+        
+        # A0 = grid.R0 @ grid.A @ grid.R0.transpose()
+        # Cratio = max(2/A0.shape[0], 12*((A0.shape[0]/600)**0.5)/A0.shape[0])
+        # Cgrid = Grid_PWA(A0, grid.mesh,Cratio)  
+        # grid = torch.load('Data/testdata/grid9.pth')
+        # grid.global_Lap_eig()
+        
+        print('Grid Made!')
+    
+        the_types = 1
+            
+        if the_types == 0:
+    
+            grid.aggop_gen(0.02, 1)
+    
+        if the_types == 2:
+    
+            grid.aggop_gen(35/grid.A.shape[0], 1)
+        
+        # grid = torch.load('data/Grids-Helmholtz-Dirichlet/grid90.pth')
+        # gg = Grid_PWA(grid.A, grid.mesh, ratio = grid.ratio, hops=1)
+        # gg.aggop_gen(grid.ratio, grid.cut, grid.aggop)
+        # grid =  gg
+        
+        if test_args.plot:
+    
+            grid.plot_agg(size = 0.0, labeling = False, w = 0.1, shade=0.0008)
+            # plt.savefig('../Paper-Multilevel-MLORAS/1_submitted_paper/figures/overTgrid.pdf', bbox_inches = 'tight')
+    
+            plt.show()
+    
+        ts = time.time()
+    
+        from NeuralNet import *
+        from hgnn import HGNN
+        model = HGNN(lvl=2, dim_embed=128, num_layers=2, K= 2, ratio=0.025, lr=1e-4)
+        # model = mloras_net(dim = 128, K = 2, num_res = 8, num_convs = 4, lr = 1e-3, res = True, tf=False)
+        
+        # directory  = 'Models/Model-TG5k-new/model_epoch_best.pth'
+        directory  = 'Models/Model-800-1k-neigh/model_epoch_best.pth'
+    
+        
+        
+        # from PrevNeuralNet import mloras_net
+        # model = mloras_net(dim = test_args.dim, K = test_args.TAGConv_k, num_res = 8, num_convs = 4, lr = 1e-4)
+    
+        # directory  = 'Models/Model-Grids-Helmholtz-Dirichlet/trained_model.pth'
+    
+        # from NN_experiment import FC_test
+        # model = FC_test(grid.A.shape[0], grid.gmask.nonzero()[0].shape[0], 128, lr = 1e-4)
+    
+        # directory  = 'Models/Model-gnn-all/model_epoch_best.pth'
+        # directory  = 'Models/Model-NN-test/model_epoch989.pth'
+    
+    
+        model.load_state_dict(torch.load(directory, map_location=torch.device('cpu')))
+        
+        list_test = ['RAS', 'ML_ORAS']
+        list_label = {'RAS':'RAS','ML_ORAS': 'MLORAS'}
+        dict_precs = {}
+        dict_stats = {}
+        n = grid.aggop[0].shape[0]
+        
+        x0 = np.random.random(grid.A.shape[0])
+        x0 = x0/((grid.A@x0)**2).sum()**0.5
+        
+        data = grid.gdata
+        data.edge_attr = data.edge_attr.float()
+        model.eval()
+        with torch.no_grad():
+            out = model(data, grid)
+        # =============================================================================
+        #     
+        # =============================================================================
+        model_R = out[1].detach().to_dense().numpy()
+    
+        grid_R = grid.R0.toarray()
+    
+        plt.figure()
+        start_col = 112
+        rows = grid_R.shape[0]
+        plt.imshow(grid_R[:rows, start_col:start_col+rows],cmap='seismic', 
+                    interpolation='nearest', extent=[start_col,start_col+rows-1, 0,rows-1],
+                    vmin = -abs(grid_R[:rows, start_col:start_col+rows]).max(), 
+                    vmax = abs(grid_R[:rows, start_col:start_col+rows]).max())
+    
+        plt.colorbar()
+        plt.show()
+    
+        plt.figure()
+        plt.imshow(model_R[:rows, start_col:start_col+rows],cmap='seismic', 
+                    interpolation='nearest', extent=[start_col,start_col+rows-1, 0,rows-1],
+                    vmin = -abs(model_R[:rows, start_col:start_col+rows]).max(), 
+                    vmax = abs(model_R[:rows, start_col:start_col+rows]).max())
+    
+        plt.colorbar()
+        plt.show()
+        # =============================================================================
+        #     
+        # =============================================================================
+        print('Passed to the network!')
         for name in list_test:
             
-            plt.plot(dict_loss[name][:-2], label = list_label[name], marker='.')
-     
-
-        plt.xlabel("fGMRES Iteration")
-        plt.ylabel("Residual norm")
-        plt.yscale('log')
-        plt.legend()
-        plt.title('FGMRES '+str(int(grid.A.shape[0]))+'-node, '+str(grid.aggop[0].shape[-1])+' aggregates')
-        # plt.savefig('../Paper-Multilevel-MLORAS/1_submitted_paper/figures/fgmres_interface.pdf', bbox_inches = 'tight')
-
-        plt.show()
-        plt.figure()
-    
-    if test_args.stationary:
-        t5 = time.time()
-        dict_enorm = test_stats(grid, dict_stats, list_test)
-        t6 = time.time()
-        print(f't65 = {t6-t5}\n')
-        for name in list_test:
+            if num_lvl == 1:
+                M = preconditioner(grid, out, precond_type=name, u = torch.tensor(x0).unsqueeze(1))
+                
+                dict_precs[name] = M.numpy()
+                dict_stats[name] = M
             
-            plt.plot(dict_enorm[name], label = list_label[name], marker='.')
+            if num_lvl == 2:
+                M = preconditioner(grid, out, precond_type=name, u = torch.tensor(x0).unsqueeze(1))
+                if name == 'ML_ORAS':
+                    dict_precs[name] = [M.to_dense().detach().numpy(), out[1]]
+                else:
+                    dict_precs[name] = [M.to_dense().detach().numpy(), None]
+                    
+                dict_stats[name] = [M, out[1]]
+    
+    
+            if num_lvl == 3:
+                
+                grids = [grid, Cgrid]
+                
+                Cdata = Cgrid.gdata
+                Cdata.edge_attr = Cdata.edge_attr.float()
+                model.eval()
+                with torch.no_grad():
+                    Cout = model(Cdata, Cgrid)
+                    
+                    
+                outs  = [out, Cout]
+                
+                M = preconditioner(grids, outs, precond_type=name, u = torch.tensor(x0).unsqueeze(1))
+                
+                dict_precs[name] = M.numpy()
+                dict_stats[name] = M
+            
+        print('Obtained the preconditioners!')
+        if test_args.precond:
+            t3 = time.time()
+            dict_loss = test_fgmres(grid, dict_precs, list_test)
+            t4 = time.time()
+            print(f't43 = {t4-t3}\n')
+            for name in list_test:
+                
+                plt.plot(dict_loss[name][:-2], label = list_label[name], marker='.')
+         
+    
+            plt.xlabel("fGMRES Iteration")
+            plt.ylabel("Residual norm")
+            plt.yscale('log')
+            plt.legend()
+            plt.title('FGMRES '+str(int(grid.A.shape[0]))+'-node, '+str(grid.aggop[0].shape[-1])+' aggregates')
+            # plt.savefig('../Paper-Multilevel-MLORAS/1_submitted_paper/figures/fgmres_interface.pdf', bbox_inches = 'tight')
+    
+            plt.show()
+            plt.figure()
         
-
-        tf = time.time()
-        print('start-end = ', tf-ts)
-        plt.xlabel("Iteration")
-        plt.ylabel("error norm")
-        plt.yscale('log')
-        # plt.ylim([1e-3, 1])
-        plt.title('Stationary '+str(int(grid.A.shape[0]))+'-node, '+str(grid.aggop[0].shape[-1])+' aggregates')
-        plt.legend()
-        # plt.savefig('../Paper-Multilevel-MLORAS/1_submitted_paper/figures/stationary_interface.pdf', bbox_inches = 'tight')
-
-        plt.show()
-        plt.figure()
+        if test_args.stationary:
+            t5 = time.time()
+            dict_enorm = test_stats(grid, dict_stats, list_test)
+            t6 = time.time()
+            print(f't65 = {t6-t5}\n')
+            for name in list_test:
+                
+                plt.plot(dict_enorm[name], label = list_label[name], marker='.')
+            
+    
+            tf = time.time()
+            print('start-end = ', tf-ts)
+            plt.xlabel("Iteration")
+            plt.ylabel("error norm")
+            plt.yscale('log')
+            plt.ylim([min(1e-5, dict_enorm['ML_ORAS'][-1]), 1])
+            plt.title('Sum != 1 Stationary '+str(int(grid.A.shape[0]))+'-node, '+str(grid.aggop[0].shape[-1])+' aggregates')
+            plt.legend()
+            # plt.savefig('../Paper-Multilevel-MLORAS/1_submitted_paper/figures/stationary_interface.pdf', bbox_inches = 'tight')
+    
+            plt.show()
+            plt.figure()
 
  
 def test_on_list():

train.py
@@ -24,7 +24,7 @@ from grids import *
 import time
 # mpl.rcParams['figure.dpi'] = 300
 from ST_CYR import *
-import numml.sparse as spml
+# import numml.sparse as spml
 import argparse
 
 from torch_geometric.loader import DataLoader
@@ -48,10 +48,10 @@ print(device)
 print('********')
 train_parser = argparse.ArgumentParser(description='Settings for training machine learning for ORAS')
 
-train_parser.add_argument('--num-epoch', type=int, default=100, help='Number of training epochs')
-train_parser.add_argument('--mini-batch-size', type=int, default=5, help='Coarsening ratio for aggregation')
+train_parser.add_argument('--num-epoch', type=int, default=200, help='Number of training epochs')
+train_parser.add_argument('--mini-batch-size', type=int, default=10, help='Coarsening ratio for aggregation')
 train_parser.add_argument('--lr', type=float, default= 1e-4, help='Learning rate')
-train_parser.add_argument('--TAGConv-k', type=int, default=6, help='TAGConv # of hops')
+train_parser.add_argument('--TAGConv-k', type=int, default=2, help='TAGConv # of hops')
 train_parser.add_argument('--dim', type=int, default=128, help='Dimension of TAGConv filter')
 train_parser.add_argument('--data-set', type=str, default='TrainingGrids', help='Directory of the training data')
 train_parser.add_argument('--K', type=int, default=10, help='Number of iterations in the loss function')
@@ -82,7 +82,7 @@ if num_lvl == 2:
 if __name__ == "__main__":
     
         
-    path = 'Models/Model-TG-new'#+train_args.data_set
+    path = 'Models/Model-TrainingGrids-neigh'#+train_args.data_set
     
     if not os.path.exists(path):
         os.makedirs(path)
@@ -90,10 +90,10 @@ if __name__ == "__main__":
     list_grids = []
     
     # num_data = sum((len(f) for _, _, f in os.walk('Data/'+train_args.data_set)))-1
-    num_data = 1
+    num_data = 100
     for i in range(num_data):
-        g = torch.load('grid1.pth')
-        # g = torch.load('Data/'+train_args.data_set+"/grid"+str(i)+".pth")
+        # g = torch.load('grid_8k.pth')
+        g = torch.load('Data/'+train_args.data_set+"/grid"+str(i)+".pth")
     
         list_grids.append(g)
     
@@ -105,7 +105,7 @@ if __name__ == "__main__":
     # list_grids = [g]
     print('Finished Uploading Training Data')
     # model = FC_test(g.A.shape[0], g.gmask.nonzero()[0].shape[0], 128, lr = 1e-4)
-    model = HGNN(lvl=2, dim_embed=128, num_layers=6, K=train_args.TAGConv_k, ratio=0.05, lr=1e-4)
+    model = HGNN(lvl=2, dim_embed=128, num_layers=2, K=train_args.TAGConv_k, ratio=0.05, lr=1e-4)
     # model = mloras_net(dim = 128, K = train_args.TAGConv_k, num_res = 8, num_convs = 4, lr = train_args.lr, res = True, tf=False)
     # model.load_state_dict(torch.load("/Users/alitaghibakhshi/PycharmProjects/ML_OSM/OSM_ML/All_Models/Models_for_Grids_pretrain3/model_epoch99.pth"))
     model.to(device)
@@ -142,8 +142,8 @@ if __name__ == "__main__":
                 data = grid.gdata.to(device)
                 data.edge_attr = data.edge_attr.float()
                 output = model.forward(data, grid)
-                sys.exit()
-                u = torch.rand(grid.x.shape[0],100).double().to(device)
+
+                u = torch.rand(grid.x.shape[0],10).double().to(device)
                 u = u/(((u**2).sum(0))**0.5).unsqueeze(0)
                 
                 current_loss = stationary_max(grid, output, u = u, K = train_args.K, precond_type='ML_ORAS')
@@ -169,6 +169,7 @@ if __name__ == "__main__":
         
         if epoch_loss < current_best_loss:
             torch.save(model.state_dict(), path+"/model_epoch_best.pth")
+            torch.save(model.state_dict(), path+"/model_epoch"+str(epoch)+".pth")   
             current_best_loss = epoch_loss
             torch.save(epoch_loss_list, path+"/loss_list.pth")
         epoch_loss = 0